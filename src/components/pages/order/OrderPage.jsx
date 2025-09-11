a2c8fa73ffcd2d01a7145632cd17276201c3efc2
BilelMat <matmouribilell@gmail.com>
2025-09-11 09:18:10 +0200

    ♻️ F02 : LOGIN > Refacto

--- src/components/pages/order/OrderPage.jsx
+++ src/components/pages/order/OrderPage.jsx
@@ -2,12 +2,13 @@ import { Link, useParams } from "react-router-dom";
 
 export default function OrderPage() {
   //etat
-  const { inputValue } = useParams();
+  const { username } = useParams();
+
   //comportement
   //affichage
   return (
     <div>
-      <h1>Bonjour {inputValue} </h1>
+      <h1>Bonjour {username} </h1>
       <br />
       <Link to="/">
         <button>Déconnexion</button>
