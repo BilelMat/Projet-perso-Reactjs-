ede183fd3af7824f55fcf433905f505490981f1e
BilelMat <matmouribilell@gmail.com>
2025-09-16 15:31:28 +0200

    💄 F03 : LOGIN > LoginForm > Add style

--- src/components/pages/login/LoginForm.jsx
+++ src/components/pages/login/LoginForm.jsx
@@ -19,7 +19,7 @@ export default function LoginForm() {
   return (
     <LoginFormStyled action="submit" onSubmit={handleSubmit}>
       <h1>Bienvenue chez nous !</h1>
-      <br />
+      <hr />
       <h2>Connectez-vous</h2>
       <input
         value={inputValue}
@@ -35,4 +35,28 @@ export default function LoginForm() {
 
 const LoginFormStyled = styled.form`
   background: green;
+  text-align: center;
+  max-width: 500px;
+  min-width: 400px;
+  margin: 0px auto;
+  padding: 2.5rem 2.5rem;
+  border-radius: 5px;
+  font-family: "Amatic SC", cursive;
+
+  h1 {
+    color: white;
+    font-size: 48px;
+  }
+
+  hr {
+    border: 1.5px solid orangered;
+    margin-bottom: 40px;
+  }
+
+  h2 {
+    border: 1px solid red;
+    color: white;
+    font-size: 36px;
+    margin: 20px 10px 10px;
+  }
 `;
