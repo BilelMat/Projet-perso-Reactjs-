548cc1e0197dc9522a061bc8ead8bca3a5507fdf
BilelMat <matmouribilell@gmail.com>
2025-09-23 11:08:07 +0200

    ♻️ F03 : LOGIN > LoginForm : Move TextInput to reusable-ui

--- src/components/pages/login/LoginForm.jsx
+++ src/components/pages/login/LoginForm.jsx
@@ -3,7 +3,7 @@ import styled from "styled-components";
 import { useNavigate } from "react-router-dom";
 import { IoChevronForward } from "react-icons/io5";
 import { BsPersonCircle } from "react-icons/bs";
-import TextInput from "./TextInput";
+import TextInput from "../../reusable-ui/TextInput";
 
 export default function LoginForm() {
   //états
